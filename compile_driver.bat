d26f353adc8ae9a8cb9bdf500ce840973c5702cb
ericrqs <eric.r@quali.com>
2017-01-30 11:57:54 -0500

    initial commit - clean up, support 7k

--- compile_driver.bat
+++ compile_driver.bat
@@ -1,10 +1,25 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo ok
+) else (
+    echo This program must be run from an administrator cmd prompt
+    goto :fail
+)
+
+@echo on
+
 pyinstaller --onefile driver.spec
 
-taskkill /f /im Telescent.exe
+taskkill /f /im Polatis.exe
 
 sleep 3
 
-copy dist\Telescent.exe                   "c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
-copy telescent_runtime_configuration.json "c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
+copy dist\Polatis.exe                     "c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
+copy polatis_runtime_configuration.json "c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
 
+copy polatis_datamodel.xml               release\
+copy dist\Polatis.exe                      release\
+copy polatis_runtime_configuration.json  release\
 
+:fail
